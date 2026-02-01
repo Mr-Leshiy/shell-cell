1768fdb1954f05ac0875ee38bf656c2a07559d40
Mr-Leshiy <leshiy12345678@gmail.com>
2026-02-01 11:27:17 +0100

    added build step for all necessary BuildKit proto files

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,13 @@
 #![doc = include_str!("../README.md")]
 #![allow(dead_code)]
 
-use std::path::{Path};
-
-use crate::{scell::SCell, scell_file::SCellFile};
-
+mod proto;
 mod scell;
 mod scell_file;
+
+use std::path::Path;
+
+use crate::{scell::SCell, scell_file::SCellFile};
 
 fn main() -> anyhow::Result<()> {
     let path = Path::new("scell.yml");
---
+++ src/proto.rs
@@ -0,0 +1,38 @@
+//! Generated protobuf code for BuildKit gRPC API
+
+pub mod moby {
+    pub mod buildkit {
+        pub mod v1 {
+            pub mod frontend {
+                tonic::include_proto!("moby.buildkit.v1.frontend");
+            }
+
+            pub mod apicaps {
+                tonic::include_proto!("moby.buildkit.v1.apicaps");
+            }
+
+            pub mod types {
+                tonic::include_proto!("moby.buildkit.v1.types");
+            }
+            pub mod sourcepolicy {
+                tonic::include_proto!("moby.buildkit.v1.sourcepolicy");
+            }
+        }
+    }
+}
+
+pub mod fsutil {
+    pub mod types {
+        tonic::include_proto!("fsutil.types");
+    }
+}
+
+pub mod google {
+    pub mod rpc {
+        tonic::include_proto!("google.rpc");
+    }
+}
+
+pub mod pb {
+    tonic::include_proto!("pb");
+}
