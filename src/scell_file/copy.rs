284e6ce708dc50dbc6ed454f2b1c32152c53093a
Mr-Leshiy <leshiy12345678@gmail.com>
2026-02-03 19:50:02 +0100

    add CopyStmt tests

--- src/scell_file/copy.rs
+++ src/scell_file/copy.rs
@@ -1,17 +1,63 @@
-use std::path::PathBuf;
+use std::{ops::Deref, path::PathBuf};
 
 #[derive(Debug, Clone, Default, PartialEq, Eq, Hash)]
+#[derive(serde::Deserialize)]
 pub struct CopyStmt(pub Vec<Vec<PathBuf>>);
 
-impl<'de> serde::Deserialize<'de> for CopyStmt {
+#[derive(Debug, Clone, Default, PartialEq, Eq, Hash)]
+pub struct CopyStmtEntry(Vec<PathBuf>);
+
+impl Deref for CopyStmtEntry {
+    type Target = Vec<PathBuf>;
+
+    fn deref(&self) -> &Self::Target {
+        &self.0
+    }
+}
+
+impl From<&str> for CopyStmtEntry {
+    fn from(s: &str) -> Self {
+        Self(s.split_whitespace().map(PathBuf::from).collect())
+    }
+}
+
+impl<'de> serde::Deserialize<'de> for CopyStmtEntry {
     fn deserialize<D>(deserializer: D) -> Result<Self, D::Error>
     where D: serde::Deserializer<'de> {
-        let copy = Vec::<String>::deserialize(deserializer)?;
+        let str = String::deserialize(deserializer)?;
+        Ok(str.as_str().into())
+    }
+}
 
-        Ok(Self(
-            copy.into_iter()
-                .map(|s| s.split_whitespace().map(PathBuf::from).collect())
-                .collect(),
-        ))
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    use std::path::PathBuf;
+
+    use test_case::test_case;
+
+    #[test_case("entry" => CopyStmtEntry(
+        vec![
+            PathBuf::from("entry")
+        ]
+    )
+    ; "one entry")]
+    #[test_case("entry1 entry2" => CopyStmtEntry(
+        vec![
+            PathBuf::from("entry1"),
+            PathBuf::from("entry2"),
+        ]
+    )
+    ; "two entries")]
+    #[test_case("       entry1     entry2      " => CopyStmtEntry(
+        vec![
+            PathBuf::from("entry1"),
+            PathBuf::from("entry2"),
+        ]
+    )
+    ; "two entries more spaces")]
+    fn test_from_parsing(input: &str) -> CopyStmtEntry {
+       input.into()
     }
 }
