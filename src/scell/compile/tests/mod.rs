9e63439b689987e16699b27308095c2b1e46856c
Mr-Leshiy <leshiy12345678@gmail.com>
2026-02-05 23:48:51 +0100

    added simple tests

--- src/scell/compile/tests/mod.rs
+++ src/scell/compile/tests/mod.rs
@@ -3,19 +3,23 @@ use std::path::{Path, PathBuf};
 use test_case::test_case;
 
 use crate::scell::{
-    parser::{name::TargetName, shell::ShellStmt}, Link, SCell
+    Link, SCell,
+    parser::{
+        build::BuildStmt, copy::CopyStmt, name::TargetName, shell::ShellStmt,
+        workspace::WorkspaceStmt,
+    },
 };
 
 #[test_case(
-    "simple.yml", None 
+    "simple_default_target.yml", None 
     => SCell {
         links: vec![
             Link::Node {
                 name: "main".parse().unwrap(),
-                location: PathBuf::from("src/scell/compile/tests/simple.yml"),
-                workspace: Default::default(),
-                copy: Default::default(),
-                build: Default::default(),
+                location: PathBuf::from("src/scell/compile/tests/simple_default_target.yml"),
+                workspace: WorkspaceStmt::default(),
+                copy: CopyStmt::default(),
+                build: BuildStmt::default(),
             },
             Link::Root("from".parse().unwrap())
             ],
@@ -24,9 +28,31 @@ use crate::scell::{
             commands: vec![]
         },
         hang: "hang".to_string(),
-    } 
-    ; "simple"
+    }
+    ; "simple default target"
 )]
+#[test_case(
+    "simple_other_target.yml", Some("other".parse().unwrap()) 
+    => SCell {
+        links: vec![
+            Link::Node {
+                name: "other".parse().unwrap(),
+                location: PathBuf::from("src/scell/compile/tests/simple_other_target.yml"),
+                workspace: WorkspaceStmt::default(),
+                copy: CopyStmt::default(),
+                build: BuildStmt::default(),
+            },
+            Link::Root("from".parse().unwrap())
+            ],
+        shell: ShellStmt {
+            bin_path: "shell".to_string(),
+            commands: vec![]
+        },
+        hang: "hang".to_string(),
+    }
+    ; "simple other target"
+)]
+
 fn compile_ok_test(
     path: &str,
     target: Option<TargetName>,
