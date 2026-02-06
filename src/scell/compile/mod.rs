8eee45a6f5d57c5f2c0f8d89d1f96710bcdf28c0
Alex Pozhylenkov <leshiy12345678@gmail.com>
2026-02-06 10:27:07 +0100

    Added tests for compilation step, properly resolve `from` statement location (#3)
    * move compile under its own directory
    
    * added first simple compile test
    
    * added simple tests
    
    * fix, added more test cases
    
    * improve error message
    
    * update CI

--- src/scell/compile/mod.rs
+++ src/scell/compile/mod.rs
@@ -1,3 +1,6 @@
+#[cfg(test)]
+mod tests;
+
 use std::path::Path;
 
 use color_eyre::eyre::Context;
@@ -70,9 +73,17 @@ impl SCell {
                 },
                 FromStmt::TargetRef { location, name } => {
                     if let Some(location) = location {
+                        let location = walk_f.location.join(location);
+                        let location = std::fs::canonicalize(&location)
+                            .user_err(format!(
+                                "Cannot resolve a directory location at {} while processing 'from' statement for target '{name}' at '{}'",
+                                location.display(),
+                                walk_f.location.display()
+                            ))?;
                         walk_f = SCellFile::from_path(&location).user_err(format!(
-                            "Fail to process 'from' statement for target '{name}' at '{}'",
-                            location.display()
+                            "Cannot find load Shell-Cell file at '{}' while processing 'from' statement for target '{name}' at '{}'",
+                            location.display(),
+                            walk_f.location.display()
                         ))?;
                     }
 
