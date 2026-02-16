800917eadce5beda514740491dce8c546154ecf7
Mr-Leshiy <leshiy12345678@gmail.com>
2026-02-16 10:50:03 +0100

    wip

--- src/pty/tests.rs
+++ src/pty/tests.rs
@@ -1,14 +1,52 @@
-use std::time::Duration;
+use std::{time::Duration};
+use std::fmt::Write;
 
 use bollard::container::LogOutput;
 use bytes::Bytes;
 use test_case::test_case;
 use tokio::io::AsyncReadExt;
+use indoc::indoc;
 
 use crate::pty::Pty;
 
-const SCREEN_SIZE: u16 = 10;
+const SCREEN_SIZE_WIDTH: u16 = 10;
+const SCREEN_SIZE_HEIGHT: u16 = 3;
 
+// -----
+// Control test cases
+// -----
+#[test_case(
+    &[
+        b"\x1B[1;5H",
+        b"\x08"
+    ]
+    =>
+    (
+        indoc!{"
+        |__________|
+        |__________|
+        |__________|
+        "}.to_string(),
+        (0, 3),
+    )
+    ;
+    "Backspace" // <https://ghostty.org/docs/vt/control/bs>
+)]
+#[test_case(
+    &[
+        b"\x1B[11G", // move to last column
+        b"A",
+        // b"\r",
+        b"X"
+    ]
+    =>
+    (
+        "X        A".to_string(),
+        (1, 0),
+    )
+    ;
+    "CR V-1: Pending Wrap is Unset" // <https://ghostty.org/docs/vt/control/cr#cr-v-1:-pending-wrap-is-unset>
+)]
 // -----
 // ESC test cases
 // -----
@@ -16,7 +54,11 @@ const SCREEN_SIZE: u16 = 10;
     &[b"\x1B#8"]
     =>
     (
-        vec!['E'; usize::from(SCREEN_SIZE * SCREEN_SIZE)].into_iter().collect::<String>(),
+        indoc!{"
+        |EEEEEEEEEE|
+        |EEEEEEEEEE|
+        |EEEEEEEEEE|
+        "}.to_string(),
         (0, 0),
     )
     ;
@@ -34,7 +76,11 @@ const SCREEN_SIZE: u16 = 10;
     ]
     =>
     (
-        "B   AX".to_string(),
+        indoc!{"
+        |B___AX____|
+        |__________|
+        |__________|
+        "}.to_string(),
         (0, 6),
     )
     ;
@@ -48,7 +94,11 @@ const SCREEN_SIZE: u16 = 10;
     ]
     =>
     (
-        "A\n X".to_string(),
+        indoc!{"
+        |A_________|
+        |_X________|
+        |__________|
+        "}.to_string(),
         (1, 2),
     )
     ;
@@ -56,16 +106,19 @@ const SCREEN_SIZE: u16 = 10;
 )]
 #[test_case(
     &[
-        b"A\r\n",        // print A + newline
-        b"B\r\n",        // print B + newline
-        b"C\r\n",        // print C + newline
+        b"A\r\n",      // print A + newline
+        b"B\r\n",      // print B + newline
         b"\x1B[1;1H",  // move to top-left
         b"\x1BM",      // RI - reverse index (move up one line, scroll if at top)
         b"X",          // print X
     ]
     =>
     (
-        "X\nA\nB\nC".to_string(),
+        indoc!{"
+        |X_________|
+        |A_________|
+        |B_________|
+        "}.to_string(),
         (0, 1),
     )
     ;
@@ -82,7 +135,11 @@ const SCREEN_SIZE: u16 = 10;
     ]
     =>
     (
-        "X\nB\nC".to_string(),
+        indoc!{"
+        |X_________|
+        |B_________|
+        |C_________|
+        "}.to_string(),
         (0, 1),
     )
     ;
@@ -91,7 +148,35 @@ const SCREEN_SIZE: u16 = 10;
 // -----
 // CSI test cases
 // -----
-// TODO: implement
+// #[test_case(
+//     &[
+//         b"\x1B[10Z",
+//         b"A",
+//     ]
+//     =>
+//     (
+//         "A".to_string(),
+//         (0, 1),
+//     )
+//     ;
+//     "CBT V-1: Left Beyond First Column" // <https://ghostty.org/docs/vt/csi/cbt#cbt-v-1:-left-beyond-first-column>
+// )]
+// #[test_case(
+//     &[
+//         b"\x1B[1;9H",
+//         b"X",
+//         b"\x1B[1;9H",
+//         b"\x1B[Z",
+//         b"A",
+//     ]
+//     =>
+//     (
+//         "A       X".to_string(),
+//         (0, 9),
+//     )
+//     ;
+//     "CBT V-2: Left Starting After Tab Stop" // <https://ghostty.org/docs/vt/csi/cbt#cbt-v-2:-left-starting-after-tab-stop>
+// )]
 #[tokio::test(flavor = "multi_thread", worker_threads = 2)]
 async fn pty_test(stdout: &'static [&[u8]]) -> (String, (u16, u16)) {
     const TIMEOUT: Duration = Duration::from_secs(1);
@@ -103,12 +188,27 @@ async fn pty_test(stdout: &'static [&[u8]]) -> (String, (u16, u16)) {
         })
     })));
     let mut pty = Pty::new("test_session".to_string(), output, input);
-    pty.set_size(SCREEN_SIZE, SCREEN_SIZE);
+    pty.set_size(SCREEN_SIZE_HEIGHT, SCREEN_SIZE_WIDTH);
 
     for _ in stdout {
         assert!(!pty.process_stdout_and_stderr(TIMEOUT));
     }
-    (pty.screen().contents(), pty.screen().cursor_position())
+
+    let mut res = String::new();
+    let (rows, cols) =  pty.size();
+    for i in 0..rows {
+        write!(&mut res, "|").unwrap();
+        for j in 0..cols {
+            let cell = pty.screen().cell(i, j).unwrap();
+            if cell.has_contents() {
+                write!(&mut res, "{}", cell.contents()).unwrap();
+            } else {
+                write!(&mut res, "_").unwrap();
+            }
+        }
+        writeln!(&mut res, "|").unwrap();
+    }
+    (res, pty.screen().cursor_position())
 }
 
 #[test_case(
@@ -142,7 +242,7 @@ async fn pty_with_response_test(stdout: &'static [&[u8]]) -> Vec<u8> {
         })
     })));
     let mut pty = Pty::new("test_session".to_string(), output, input);
-    pty.set_size(SCREEN_SIZE, SCREEN_SIZE);
+    pty.set_size(SCREEN_SIZE_HEIGHT, SCREEN_SIZE_WIDTH);
 
     for _ in stdout {
         assert!(!pty.process_stdout_and_stderr(TIMEOUT));
