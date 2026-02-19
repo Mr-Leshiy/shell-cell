5081ed3bb41ed327897e097aa13bbe2cf288f53e
Alex Pozhylenkov <leshiy12345678@gmail.com>
2026-02-19 15:43:46 +0100

    test: `SCell` compilation error test cases (#70)

--- src/scell/compile/tests/err/mod.rs
+++ src/scell/compile/tests/err/mod.rs
@@ -7,7 +7,8 @@ use crate::{
     scell::{
         SCell,
         compile::errors::{
-            CircularTargets, MissingEntrypoint, MissingHangStmt, MissingShellStmt, MissingTarget,
+            CircularTargets, CopySrcNotFound, DirNotFoundFromStmt, DockerfileNotFound,
+            FileLoadFromStmt, MissingEntrypoint, MissingHangStmt, MissingShellStmt, MissingTarget,
         },
         types::name::TargetName,
     },
@@ -39,6 +40,42 @@ const ERR_FIXTURES: &str = "src/scell/compile/tests/err";
     "missing_hang_stmt", None
     => MissingHangStmt
     ; "missing hang stmt"
+)]
+#[test_case(
+    "dockerfile_not_found", None
+    => DockerfileNotFound(
+        std::path::PathBuf::from("Dockerfile"),
+        "main".parse().unwrap(),
+        std::fs::canonicalize(Path::new(ERR_FIXTURES).join("dockerfile_not_found")).unwrap()
+    )
+    ; "dockerfile not found"
+)]
+#[test_case(
+    "copy_src_not_found", None
+    => CopySrcNotFound(
+        std::path::PathBuf::from("nonexistent.txt"),
+        "main".parse().unwrap(),
+        std::fs::canonicalize(Path::new(ERR_FIXTURES).join("copy_src_not_found")).unwrap()
+    )
+    ; "copy src not found"
+)]
+#[test_case(
+    "dir_not_found_from_stmt", None
+    => DirNotFoundFromStmt(
+        std::path::PathBuf::from("nonexistent/path"),
+        "other".parse().unwrap(),
+        std::fs::canonicalize(Path::new(ERR_FIXTURES).join("dir_not_found_from_stmt")).unwrap()
+    )
+    ; "dir not found from stmt"
+)]
+#[test_case(
+    "file_load_from_stmt", None
+    => FileLoadFromStmt(
+        std::fs::canonicalize(Path::new(ERR_FIXTURES).join("file_load_from_stmt/empty_dir")).unwrap(),
+        "other".parse().unwrap(),
+        std::fs::canonicalize(Path::new(ERR_FIXTURES).join("file_load_from_stmt")).unwrap()
+    )
+    ; "file load from stmt"
 )]
 fn compile_err_test<E: std::error::Error + PartialEq + Sync + Send + 'static>(
     dir_path: &str,
