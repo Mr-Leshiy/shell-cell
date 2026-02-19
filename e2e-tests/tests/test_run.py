dec7f9ed0ef357aa41b082377fe866cbb43bd510
Mr-Leshiy <leshiy12345678@gmail.com>
2026-02-19 08:57:36 +0100

    fix

--- e2e-tests/tests/test_run.py
+++ e2e-tests/tests/test_run.py
@@ -55,19 +55,13 @@ def test_scell_run_mount(scell) -> None:
     child.expect("works!")
     assert_scell_stop_session(child)
 
-# TODO: fix this test for running inside CI
 def skip_test_scell_run_ports(scell) -> None:
     child = scell(args=["data"])
 
     assert_scell_prepare_session(child)
-    child.sendline("python3 -m http.server 4321")
-    child.expect("Serving")
-    child.expect("HTTP")
     import requests
     resp = requests.get("http://0.0.0.0:4321", timeout=30)
     assert resp.status_code == 200
-    # Send Ctrl-c to stop the python3 HTTP server
-    child.send('\x03')
     assert_scell_stop_session(child)
 
 
