a57152903253054a61adaff6360fd6ac510029ae
Mr-Leshiy <leshiy12345678@gmail.com>
2026-02-21 09:01:06 +0100

    fix

--- src/cli/init/mod.rs
+++ src/cli/init/mod.rs
@@ -1,28 +1,18 @@
-use std::fs;
+use crate::{cli::Cli, error::UserError, scell::types::SCELL_FILE_NAME};
 
-use crate::{cli::Cli, error::{UserError, WrapUserError}};
-
-const BLUEPRINT_FILENAME: &str = "scell.yml";
-
-const BLUEPRINT: &str = include_str!("template_scell.yml");
+const BLUEPRINT: &[u8] = include_bytes!("template_scell.yml");
 
 impl Cli {
     pub fn init(self) -> color_eyre::Result<()> {
-        let path = self.scell_path.join(BLUEPRINT_FILENAME);
-
+        let path = self.scell_path.join(SCELL_FILE_NAME);
         if path.exists() {
-            return Err(UserError::wrap(format!(
-                "`{BLUEPRINT_FILENAME}` already exists in `{}`",
+            Err(UserError::wrap(format!(
+                "`{SCELL_FILE_NAME}` already exists in `{}`",
                 self.scell_path.display()
-            ))
-            .into());
+            )))?;
         }
-
-        fs::write(&path, BLUEPRINT)
-            .wrap_user_err(format!("Failed to create `{}`", path.display()))?;
-
+        std::fs::write(&path, BLUEPRINT)?;
         println!("Created `{}`", path.display());
-
         Ok(())
     }
 }
