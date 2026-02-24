09163bb1c1fd370328ffb441a402f232c7007ff9
Mr-Leshiy <leshiy12345678@gmail.com>
2026-02-24 10:31:11 +0100

    add scroll bar

--- src/cli/ls/app/ui.rs
+++ src/cli/ls/app/ui.rs
@@ -1,11 +1,13 @@
 use std::fmt::Display;
 
 use ratatui::{
-    layout::{Constraint, HorizontalAlignment, Layout, Rect},
+    layout::{Constraint, HorizontalAlignment, Layout, Margin, Rect},
     style::{Color, Modifier, Style},
     text::{Line, Span},
     widgets::{Block, Borders, Cell, Clear, Paragraph, Row, StatefulWidget, Table, Widget, Wrap},
 };
+use tui_scrollbar::{ScrollBar, ScrollBarArrows, ScrollLengths};
+use tui_scrollview::{ScrollView, ScrollViewState};
 
 use super::{AppInner, LsState};
 use crate::buildkit::{container_info::SCellContainerInfo, image_info::SCellImageInfo};
@@ -883,20 +885,42 @@ fn render_definition_overlay(
         })
         .collect();
 
-    Widget::render(Clear, overlay_area, buf);
+    Clear.render(overlay_area, buf);
 
-    let paragraph = Paragraph::new(lines)
-        .block(
-            Block::default()
-                .borders(Borders::ALL)
-                .title(" Definition ")
-                .title_bottom("i / Esc: close this window")
-                .title_alignment(HorizontalAlignment::Center)
-                .border_style(Style::default().fg(Color::Green)),
-        )
-        .wrap(Wrap { trim: false });
+    let block = Block::default()
+        .borders(Borders::ALL)
+        .title(" Definition ")
+        .title_bottom("i / Esc: close this window")
+        .title_alignment(HorizontalAlignment::Center)
+        .border_style(Style::default().fg(Color::Cyan));
 
-    paragraph.render(overlay_area, buf);
+    let inner_overlay_arrea = overlay_area.inner(Margin::new(1, 1));
+    let inner_split = Layout::horizontal([Constraint::Percentage(100), Constraint::Length(1)])
+        .split(inner_overlay_arrea);
+    let content_area = inner_split[0];
+    let scrollbar_area = inner_split[1];
+
+    let mut state = ScrollViewState::new();
+    let mut scroll_view = ScrollView::new(content_area.as_size());
+
+    let scroll_bar_lengths = ScrollLengths {
+        content_len: lines.len(),
+        viewport_len: content_area.height.into(),
+    };
+    let paragraph = Paragraph::new(lines);
+
+    scroll_view.render_widget(
+        paragraph,
+        Rect::new(0, 0, content_area.width, content_area.height),
+    );
+    scroll_view.render(content_area, buf, &mut state);
+    if scroll_bar_lengths.content_len > scroll_bar_lengths.viewport_len {
+        let scroll_bar = ScrollBar::vertical(scroll_bar_lengths)
+            .arrows(ScrollBarArrows::Both)
+            .offset(state.offset().y.into());
+        scroll_bar.render(scrollbar_area, buf);
+    }
+    block.render(overlay_area, buf);
 }
 
 fn main_block(items_title: impl Display) -> Block<'static> {
