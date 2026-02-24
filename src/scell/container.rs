61f93c3669b6c68b349cc11ff970bfb8c8dfc313
Mr-Leshiy <leshiy12345678@gmail.com>
2026-02-24 18:22:07 +0100

    fix

--- src/scell/container.rs
+++
@@ -1,7 +0,0 @@
-use crate::scell::types::target::{config::ConfigStmt, shell::ShellStmt};
-
-#[derive(Debug, Clone, PartialEq, Eq, Hash, serde::Serialize)]
-pub struct SCellContainer {
-    shell: ShellStmt,
-    config: Option<ConfigStmt>,
-}
