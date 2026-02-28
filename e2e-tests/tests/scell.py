ee90de88730a8ad41e9f4e242f71d353b1b2ee38
Alex Pozhylenkov <leshiy12345678@gmail.com>
2026-02-28 11:12:49 +0100

    chore: Bumped dependencies, fixing e2e tests (#93)

--- e2e-tests/tests/scell.py
+++ e2e-tests/tests/scell.py
@@ -1,4 +1,5 @@
 import os
+from typing import Any
 
 import pexpect
 import pytest
@@ -7,24 +8,46 @@ SCELL_WINDOWN_WIDTH = 800
 SCELL_WINDOWN_HEIGHT = 600
 
 
-def assert_clean_exit(child: pexpect.spawn) -> None:
+class SCell:
+    def __init__(self, process: pexpect.spawn) -> None:
+        self._process = process
+        # fout = open('mylog.txt','wb')
+        # self._process.logfile = fout
+
+    @property
+    def exitstatus(self) -> int | None:
+        return self._process.exitstatus
+
+    def expect(self, pattern: Any, timeout: int | None = None) -> int:
+        if timeout is not None:
+            return self._process.expect(pattern, timeout=timeout)
+        return self._process.expect(pattern)
+
+    def send(self, s: str) -> int:
+        return self._process.send(f"{s}\r")
+
+    def close(self) -> None:
+        self._process.close()
+
+
+def assert_clean_exit(child: SCell) -> None:
     child.expect(pexpect.EOF, timeout=1)
     child.close()
     assert child.exitstatus == 0
 
 
 @pytest.fixture(scope="session")
-def scell():
+def spawn_scell():
     scell_bin = os.environ.get("SCELL_BIN")
     assert scell_bin, "Set the 'SCELL_BIN' env var with the path to the 'scell' binary on your machine"
 
-    def spawn_scell(args: list[str], timeout: int = 10) -> pexpect.spawn:
+    def spawn_scell(args: list[str], timeout: int = 10) -> SCell:
         scell_process = pexpect.spawn(
             scell_bin,
             args=args,
             dimensions=(SCELL_WINDOWN_HEIGHT, SCELL_WINDOWN_WIDTH),
             timeout=timeout,
         )
-        return scell_process
+        return SCell(scell_process)
 
     return spawn_scell
--- e2e-tests/tests/test_basic.py
+++ e2e-tests/tests/test_basic.py
@@ -1,8 +1,8 @@
-from scell import assert_clean_exit, scell
+from scell import assert_clean_exit, spawn_scell
 
 
-def test_scell_basic(scell) -> None:
-    child = scell(args=["--version"])
-    child.expect("shell-cell 1.4.0")
+def test_scell_basic(spawn_scell) -> None:
+    scell = spawn_scell(args=["--version"])
+    scell.expect("shell-cell 1.4.0")
 
-    assert_clean_exit(child)
+    assert_clean_exit(scell)
