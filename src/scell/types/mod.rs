1cbb47f8fb8aec3d7b7709f0bac0455919b4d979
Mr-Leshiy <leshiy12345678@gmail.com>
2026-03-01 13:49:12 +0100

    wip

--- src/scell/types/mod.rs
+++ src/scell/types/mod.rs
@@ -6,7 +6,6 @@ pub mod target;
 
 use std::{
     collections::HashMap,
-    io::Read,
     path::{Path, PathBuf},
 };
 
@@ -19,7 +18,7 @@ use crate::error::WrapUserError;
 
 pub const SCELL_YML_FILE_NAME: &str = "scell.yml";
 
-pub const SCELL_SCHEMA: &str = include_str!("scell_schema.cue");
+pub const SCELL_SCHEMA: &[u8] = include_bytes!("scell_schema.cue");
 
 #[derive(Debug)]
 pub struct SCellFile {
@@ -29,23 +28,39 @@ pub struct SCellFile {
 
 impl SCellFile {
     pub fn from_path<P: AsRef<Path>>(path: P) -> color_eyre::Result<Self> {
-        let schema = cue_rs::Value::new(SCELL_SCHEMA)?;
-        schema.validate()?;
+        let ctx = cue_rs::Ctx::new()?;
+        let schema = cue_rs::Value::compile_bytes(&ctx, SCELL_SCHEMA)?;
+        schema.is_valid()?;
 
         let location = std::fs::canonicalize(&path)
             .wrap_user_err(FilePathNotResolved(path.as_ref().to_path_buf()))?;
         let file_path = location.join(SCELL_YML_FILE_NAME);
 
-        let mut file: std::fs::File =
+        let file =
             std::fs::File::open(&file_path).wrap_user_err(FileOpenFailed(file_path.clone()))?;
-
-        let mut scell_yml_str = String::new();
-        file.read_to_string(&mut scell_yml_str)?;
-        schema.validate_yaml(&scell_yml_str).mark_as_user_err()?;
+        let scell_yaml: yaml_serde::Value = yaml_serde::from_reader(file)?;
+        let scell_json: serde_json::Value = serde_json::to_value(scell_yaml)?;
+        let scell_json_bytes = serde_json::to_vec(&scell_json)?;
+        let scell_cue = cue_rs::Value::compile_bytes(&ctx, &scell_json_bytes)?;
+        cue_rs::Value::unify(&schema, &scell_cue)
+            .is_valid()
+            .mark_as_user_err()?;
 
         let cells: HashMap<TargetName, TargetStmt> =
-            yaml_serde::from_str(&scell_yml_str).mark_as_user_err()?;
+            serde_json::from_value(scell_json).mark_as_user_err()?;
 
         Ok(Self { cells, location })
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn schema_validity_test() {
+        let ctx = cue_rs::Ctx::new().unwrap();
+        let schema = cue_rs::Value::compile_bytes(&ctx, SCELL_SCHEMA).unwrap();
+        schema.is_valid().unwrap();
     }
 }
