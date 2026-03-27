37deeb0ec31ce3f3f1d82fb952831070e1d53d08
Mr-Leshiy <leshiy12345678@gmail.com>
2026-03-27 16:43:42 +0100

    set the cursor visibility to false, if there is an scrolback offset

--- src/cli/run/app/running_pty/ui.rs
+++ src/cli/run/app/running_pty/ui.rs
@@ -28,6 +28,10 @@ impl Widget for &mut RunningPtyState {
 
         // set the proper size for the terminal screen
         self.pty.set_size(inner.height, inner.width);
-        tui_term::widget::PseudoTerminal::new(self.pty.screen()).render(inner, buf);
+        let is_cursor_visible = self.pty.screen().scrollback() == 0;
+        let cursor = tui_term::widget::Cursor::default().visibility(is_cursor_visible);
+        tui_term::widget::PseudoTerminal::new(self.pty.screen())
+            .cursor(cursor)
+            .render(inner, buf);
     }
 }
