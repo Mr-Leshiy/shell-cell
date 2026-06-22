0fbe8c2903f42e0b2dccfaa64bcfc80fe81af30b
leshiy <leshiy12345678@gmail.com>
2026-06-22 17:24:01 +0200

    fix clippy

--- src/cli/run/mod.rs
+++ src/cli/run/mod.rs
@@ -15,7 +15,7 @@ pub async fn run<P: AsRef<Path> + Send + 'static>(
     detach: bool,
     quiet: bool,
 ) -> color_eyre::Result<()> {
-    let scell_path = resolve_scell_path(scell_path)?;
+    let scell_path = resolve_scell_path(scell_path);
     let buildkit = BuildKitD::start().await?;
     let mut terminal = Terminal::new()?;
     let res = App::run(&buildkit, scell_path, target, detach, quiet, &mut terminal).await;
@@ -26,13 +26,13 @@ pub async fn run<P: AsRef<Path> + Send + 'static>(
 /// Resolves the blueprint path provided by the user. When the path does not contain a
 /// `scell.cue` file, falls back to the global blueprint located at
 /// `scell_home_dir()/scell.cue` if it exists.
-fn resolve_scell_path<P: AsRef<Path>>(scell_path: P) -> color_eyre::Result<PathBuf> {
+fn resolve_scell_path<P: AsRef<Path>>(scell_path: P) -> PathBuf {
     if let Ok(home_dir) = scell_home_dir()
         && home_dir.join(SCELL_CUE_FILE_NAME).is_file()
         && !scell_path.as_ref().join(SCELL_CUE_FILE_NAME).is_file()
     {
-        Ok(home_dir)
+        home_dir
     } else {
-        Ok(scell_path.as_ref().to_path_buf())
+        scell_path.as_ref().to_path_buf()
     }
 }
